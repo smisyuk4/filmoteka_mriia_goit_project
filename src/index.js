40c0cb65572660f42beb47e637391a0d943e6963
Naliv88 <nalivpv@gmail.com>
2022-11-26 00:59:42 +0200

    Translate
    сделал с сохранением в локал, чтобы при перезагрузке страницы и переходе не менялся язык

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { refs } from "./scripts/refs";
-import { Filmoteka } from './scripts/fetch-api'
+import { Filmoteka } from './scripts/fetch-api';
+import {setLagnuage, changeLagnuage } from "./test/changeLagnuage"
 
 import Notiflix from 'notiflix';
 
@@ -17,3 +18,13 @@ async function onClickSearchBtn(event) {
     const data = await filmoteka.fetchFilms()
     console.log(data)
 }
+
+
+// Функція зміни мови
+setLagnuage(); //для першого відображення тексту, по замовчуванню Eng
+
+const translateCheckboxRef = document.querySelector(".translate");
+translateCheckboxRef.addEventListener('click', changeLagnuage);
+
+// console.log(translateCheckboxRef.value, translateCheckboxRef.checked);
+// console.log(JSON.parse(localStorage.getItem("Language")).lang);
---
+++ src/test/dictionary.js
@@ -0,0 +1,17 @@
+const ua = {
+  chengeLang: "en-EN",
+  lang: "ua-UA",
+  hello: 'Привіт',
+  buttonWatched: 'Україна',
+  buttonQueue: ' Україна',
+};
+
+const eng = {
+  chengeLang: "ua-UA",
+  lang: "en-EN",
+  hello: 'Hello',
+  buttonWatched: 'English',
+  buttonQueue: 'English',
+};
+
+export { ua, eng };
