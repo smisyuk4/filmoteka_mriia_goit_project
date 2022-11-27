28e973327a1147a2a0fc80e20a2a4444cfbdc6ac
olenkasereda <106010289+olenkasereda@users.noreply.github.com>
2022-11-28 00:59:45 +0200

    markup and rendering

--- src/index.js
+++ src/index.js
@@ -1,40 +1,36 @@
-import { refs } from "./scripts/refs";
+import { refs } from './scripts/refs';
 import { Filmoteka } from './scripts/fetch-api';
-import { setLagnuage, changeLagnuage } from "./scripts/changeLagnuage"
-import './scripts/get-tranding'
-import './scripts/find-by-name'
-import './scripts/find-by-id'
-import './scripts/find-trailer'
-import './scripts/pagination'
-import './scripts/firebase-auth'
-import { MovieLibrary } from './scripts/library-scripts'
-import { FireBaseData } from './scripts/firebase-auth'
-import { hidenButton, upClick } from './scripts/scroll-up'
+import { setLagnuage, changeLagnuage } from './scripts/changeLagnuage';
+import './scripts/get-tranding';
+import './scripts/find-by-name';
+import './scripts/find-by-id';
+import './scripts/find-trailer';
+import './scripts/pagination';
+import './scripts/firebase-auth';
+import { MovieLibrary } from './scripts/library-scripts';
+import { FireBaseData } from './scripts/firebase-auth';
+import { hidenButton, upClick } from './scripts/scroll-up';
 import Notiflix from 'notiflix';
 
-window.filmoteka = new Filmoteka()
-window.fireBase = new FireBaseData
-window.movieLibrary = new MovieLibrary
+window.filmoteka = new Filmoteka();
+window.fireBase = new FireBaseData();
+window.movieLibrary = new MovieLibrary();
 
 document.querySelector('.login-btn').addEventListener('click', e => {
-    window.fireBase.login();
+  window.fireBase.login();
 });
-document.querySelector('.film-card').addEventListener('click', e => {
-    window.movieLibrary.addToQueue(505)
-});
-
-
-
+// document.querySelector('.film-card').addEventListener('click', e => {
+//     window.movieLibrary.addToQueue(505)
+// });
 
 window.addEventListener('scroll', hidenButton);
 document.querySelector('.btn-up').addEventListener('click', upClick);
 
 // Функція зміни мови
 setLagnuage(); //для першого відображення тексту, по замовчуванню Eng
-document.querySelector(".translate").addEventListener('click', () => {
-    changeLagnuage();
-    window.movieLibrary.updateLang();
+document.querySelector('.translate').addEventListener('click', () => {
+  changeLagnuage();
+  window.movieLibrary.updateLang();
 });
 // console.log(translateCheckboxRef.value, translateCheckboxRef.checked);
 // console.log(JSON.parse(localStorage.getItem("Language")).lang);
-
--- src/scripts/find-by-name.js
+++ src/scripts/find-by-name.js
@@ -1,29 +1,77 @@
 // description: https://developers.themoviedb.org/3/search/search-movies
 // example: https://api.themoviedb.org/3/search/movie?api_key=2e329d861e790504d655e6d7175d4c52&language=en-US&page=1&include_adult=false
 
-import { refs } from "/src/scripts/refs"
-import { Filmoteka } from "./fetch-api"
+import { refs } from '/src/scripts/refs';
+import { Filmoteka } from './fetch-api';
 
-refs.searchForm.addEventListener('submit', onClickSearchBtn)
+const container = document.querySelector('.table-list');
+refs.searchForm.addEventListener('submit', onClickSearchBtn);
 
 async function onClickSearchBtn(event) {
-    event.preventDefault()
-    const OPTION_SEARCH = '/search/movie'
-    const filmoteka = new Filmoteka()
+  event.preventDefault();
+  const OPTION_SEARCH = '/search/movie';
+  const filmoteka = new Filmoteka();
 
-    //get nameFilm from input
-    const nameFilm = refs.searchInput.value
-    console.log(nameFilm)
+  //get nameFilm from input
+  const nameFilm = refs.searchInput.value;
+  console.log(nameFilm);
 
-    const valueObj = {
-        option: OPTION_SEARCH,
-        nameFilm: `&query=${encodeURIComponent(nameFilm)}`,
-    }
+  const valueObj = {
+    option: OPTION_SEARCH,
+    nameFilm: `&query=${encodeURIComponent(nameFilm)}`,
+  };
 
-    try {
-        const data = await filmoteka.fetchFilms(valueObj)
-        console.log(data)
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const data = await filmoteka.fetchFilms(valueObj);
+    console.log(data.results);
+    // const results = data.results;
+    // console.log(markup);
+    clearMurkup();
+    appendFilmCardsMarkup(data.results);
+    refs.searchForm.reset;
+    // container.insertAdjacentHTML('beforeend', markup);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const BASE_IMAGES_URL = 'https://image.tmdb.org/t/p/w400';
+
+function createFilmCardMarkup(data) {
+  return data
+    .map(
+      ({
+        // id,
+        poster_path,
+        release_date,
+        genres,
+        // popularity,
+        title,
+        vote_average,
+      }) =>
+        `</li>
+      <li class="table-item film-card__item">
+        <img src="${
+          poster_path ? BASE_IMAGES_URL + poster_path : defaultPicture
+        }" alt="poster" />
+        <div class="film-card__box-info">
+          <h3 class="film-card__title">${title}</h3>
+          <p class="film-card__text">
+            ${genres} | ${release_date}<span class="film-card__rating">${vote_average}</span>
+          </p>
+        </div>
+      </li> `
+    )
+    .join('');
+}
+
+function appendFilmCardsMarkup(results) {
+  return container.insertAdjacentHTML(
+    'beforeend',
+    createFilmCardMarkup(results)
+  );
+}
+
+function clearMurkup() {
+  container.innerHTML = '';
 }
