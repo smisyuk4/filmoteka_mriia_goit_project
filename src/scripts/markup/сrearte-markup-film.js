69facf710d0e900ef6d9bd752d189059a7e84262
Sergey <51460801+smisyuk4@users.noreply.github.com>
2022-11-28 16:08:34 +0200

    Add link to hosting image and change ternary

--- src/scripts/markup/сrearte-markup-film.js
+++ src/scripts/markup/сrearte-markup-film.js
@@ -2,6 +2,8 @@ const BASE_IMAGES_URL = 'https://image.tmdb.org/t/p/w400';
 //import defaultPicture from '/src/images/test-img-card.jpg';
 
 export function createFilmCardMarkup(data) {
+const serImg = 'https://i.postimg.cc/Y0NKgxRL/CDBD31-CB-2-C43-438-F-A140-5-CBA7-C480-AB3.jpg'
+// <a href='https://postimg.cc/K3YHhBdm' target='_blank'><img src='https://i.postimg.cc/Y0NKgxRL/CDBD31-CB-2-C43-438-F-A140-5-CBA7-C480-AB3.jpg' border='0' alt='CDBD31-CB-2-C43-438-F-A140-5-CBA7-C480-AB3'/></a>
   //   const genreNames = genres.map(el => el.name).join(', ');
   return data
     .map(
@@ -17,7 +19,7 @@ export function createFilmCardMarkup(data) {
         `</li>
       <li class="table-item film-card__item">
         <img src="${
-          poster_path ? BASE_IMAGES_URL + poster_path : defaultPicture
+          poster_path ? BASE_IMAGES_URL + poster_path : serImg
         }" alt="${title}" loading="lazy" />
         <div class="film-card__box-info">
           <h3 class="film-card__title">${title}</h3>
