848aad954d084fecbdd28b74748973b6518a2675
Fredy228 <smagrovich58@gmail.com>
2022-11-29 12:53:18 +0200

    fix/style-murkap

--- src/scripts/markup/create-markup-film.js
+++ src/scripts/markup/create-markup-film.js
@@ -3,42 +3,39 @@ import { refs } from '../refs';
 
 const BASE_IMAGES_URL = 'https://image.tmdb.org/t/p/w400';
 const DEFAULT_IMG =
-    'https://i.postimg.cc/Y0NKgxRL/CDBD31-CB-2-C43-438-F-A140-5-CBA7-C480-AB3.jpg';
+  'https://i.postimg.cc/Y0NKgxRL/CDBD31-CB-2-C43-438-F-A140-5-CBA7-C480-AB3.jpg';
 
 export async function createFilmCardMarkup(data) {
   const genresArr = await getGenres();
 
   return data
     .map(
-      ({
-        id,
-        poster_path,
-        release_date,
-        genre_ids,
-        title,
-        vote_average,
-      }) => {
+      ({ id, poster_path, release_date, genre_ids, title, vote_average }) => {
         const genresList = genresArr
           .filter(e => genre_ids.includes(e.id))
           .map(e => e.name)
           .join(', ');
 
-        let imgLink
+        let imgLink;
         if (!poster_path) {
-            imgLink = DEFAULT_IMG
+          imgLink = DEFAULT_IMG;
         } else {
-            imgLink = `${BASE_IMAGES_URL}${poster_path}`
-        } 
+          imgLink = `${BASE_IMAGES_URL}${poster_path}`;
+        }
 
-        const markup = `</li>
-                          <li class="table-item film-card__item" data-id="${id}">
-                            <img src="${imgLink}" alt="${title}" loading="lazy" width="350"/>
+        const markup = `<li class="table-item film-card__item" data-id="${id}">
+                            <div class="film-card__box-img">
+                            <img class="film-card__img" src="${imgLink}" alt="${title}" loading="lazy" width="350"/>
+                            </div>
                             <div class="film-card__box-info">
                               <h3 class="film-card__title">${title}</h3>
                               <p class="film-card__text">
-                                ${genresList} | ${release_date.slice(0,4)}<span class="film-card__rating">${
-                            Math.round(vote_average * 100) / 100
-                          }</span>
+                                ${genresList} | ${release_date.slice(
+          0,
+          4
+        )}<span class="film-card__rating">${
+          Math.round(vote_average * 100) / 100
+        }</span>
                               </p>
                             </div>
                           </li> `;
